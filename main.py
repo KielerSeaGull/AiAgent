c5af433deda0d4d7bd283ebcda9a0757b88faad9
KielerSeaGull <joel@blaich.rocks>
2025-12-18 20:54:12 +0100

    Chapter 1 done: Token metadata, User Input with parser.add_argument(user_prompt), implementation of --verbose for more info

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
-if not os.environ.get("GEMINI_API_KEY") or os.environ.get("GEMINI_API_KEY") == None:
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if not api_key:
     raise RuntimeError("Es konnte kein Gemini API Schlüssel geladen werden")
-else:
-    api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
+    if not response.usage_metadata:
+        raise RuntimeError("Fehlgeschlagene API Anfrage")
+    else:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    print("Response:")
     print(response.text)
 
 
