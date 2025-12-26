e199548c8e7c3bdf3a114c9b848ad221d9b37056
KielerSeaGull <joel@blaich.rocks>
2025-12-26 11:42:53 +0100

    added a functions dir and a get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,23 @@
+import os
+
+
+def get_files_info(working_directory, directory=".") -> str:
+    working_dir_abs: str = os.path.abspath(working_directory)
+    target_dir: str = os.path.normpath(os.path.join(working_dir_abs, directory))
+    valid_target_dir: bool = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    
+    file_infos: list = []
+    try:
+        with os.scandir(target_dir) as dir:
+            for entry in dir:
+                formatted_file_info: str = f'- {entry.name}: file_size={entry.stat().st_size} bytes, is_dir={entry.is_dir()}'
+                file_infos.append(formatted_file_info)
+
+        return '\n'.join(file_infos)
+    except:
+        return "Error: Scandir(target_dir) doesn't work"
